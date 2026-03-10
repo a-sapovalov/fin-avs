92c642f92c7035c83f245256d013b395a4e89677
Wix Remote Machine <support@wix.com>
2026-03-10 15:32:31 +0000

    Restored to checkpoint 269eb13cc1a7c3f172a6ffaaabecc52e1dbd3f4e

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,6 +32,13 @@ export default function Header() {
         <div className="flex items-center justify-between bg-primary">
           {/* Logo */}
           <Link to="/" className="flex items-center gap-2 hover:opacity-80 transition-opacity duration-300">
+            <Image
+              src="https://static.wixstatic.com/media/43558d_501e7802d49f4213a0e0e044e87bae47~mv2.png"
+              width={60}
+              height={60}
+              className="h-14 w-auto"
+              originWidth={2000}
+              originHeight={2000} />
 
           </Link>
 
