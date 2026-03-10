f7cce8a50fbe1411e7f1174611a521c7f9ed8b6c
Wix Remote Machine <support@wix.com>
2026-03-10 15:12:37 +0000

    Resize logo to 200x200

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,9 +34,9 @@ export default function Header() {
           <Link to="/" className="flex items-center gap-2 hover:opacity-80 transition-opacity duration-300">
             <Image
               src="https://static.wixstatic.com/media/43558d_501e7802d49f4213a0e0e044e87bae47~mv2.png"
-              width={60}
-              height={60}
-              className="h-14 w-auto"
+              width={200}
+              height={200}
+              className="h-auto w-auto"
               originWidth={2000}
               originHeight={2000} />
 
