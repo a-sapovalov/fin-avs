50218b2916c1afd42937ab6406aecd582865abb3
Wix Remote Machine <support@wix.com>
2026-03-27 13:26:15 +0000

    Restored to checkpoint 9b5b3754be25e728edc634c7b7523e72648d5979

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,6 @@ export default function Header() {
     { path: '/loans', label: 'loans' },
     { path: '/credit-line', label: 'credit lines' },
     { path: '/leases', label: 'leases' },
-    { path: '/price-list', label: 'price list' },
     { path: '/contacts', label: 'contacts' },
   ];
 
