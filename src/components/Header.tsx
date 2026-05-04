6debf23778bfb0ac09126fcdc52f80cd96e79d90
Wix Remote Machine <support@wix.com>
2026-05-04 09:16:43 +0000

    Remove header reference

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,6 @@ export default function Header() {
     { path: '/credit-line', label: 'credit lines' },
     { path: '/contacts', label: 'contacts' },
     { path: '/faq', label: 'faq' },
-    { path: '/terms-of-service', label: 'terms of service' },
   ];
 
   const languages: { code: Language; name: string }[] = [
